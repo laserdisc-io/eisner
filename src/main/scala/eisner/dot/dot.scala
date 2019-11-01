86f71a2397b0164c0e9d6001d75ab608348390c3
Julien Sirocchi <julien.sirocchi@gmail.com>
2019-11-01 08:45:52 +0000

    rename patterns + reformat

--- src/main/scala/eisner/dot/dot.scala
+++ src/main/scala/eisner/dot/dot.scala
@@ -1,11 +1,11 @@
 package eisner
 
 package object dot {
-  private[this] final val SubCat = "\\s*(Sub-topology: ([0-9]*)).*".r
-  private[this] final val Source = "\\s*Source:\\s+(\\S+)\\s+\\(topics:\\s\\[(.*)\\]\\)".r
-  private[this] final val Proc   = "\\s*Processor:\\s+(\\S+)\\s+\\(stores:\\s\\[(.*)\\]\\)".r
-  private[this] final val Sink   = "\\s*Sink:\\s+(\\S+)\\s+\\(topic:\\s(.*)\\)".r
-  private[this] final val Arrow  = "\\s*-->\\s+(.*)".r
+  private[this] final val SubTopology = "\\s*(Sub-topology: ([0-9]*)).*".r
+  private[this] final val Source      = "\\s*Source:\\s+(\\S+)\\s+\\(topics:\\s\\[(.*)\\]\\)".r
+  private[this] final val Processor   = "\\s*Processor:\\s+(\\S+)\\s+\\(stores:\\s\\[(.*)\\]\\)".r
+  private[this] final val Sink        = "\\s*Sink:\\s+(\\S+)\\s+\\(topic:\\s(.*)\\)".r
+  private[this] final val RightArrow  = "\\s*-->\\s+(.*)".r
 
   private[this] final object Clean {
     final def unapply(s: String): Option[String] = {
@@ -19,15 +19,15 @@ package object dot {
 
   final def toDot(s: String): DiGraph = {
     val (g, _) = s.split('\n').foldLeft(DiGraph.empty -> (None: Option[String])) {
-      case ((DiGraph(sgs, es, ts, ss), maybeN), SubCat(Clean(sc), id)) =>
+      case ((DiGraph(sgs, es, ts, ss), maybeN), SubTopology(Clean(sc), id)) =>
         DiGraph(Subgraph.empty(id, sc) :: sgs, es, ts, ss) -> maybeN
       case ((DiGraph(sgs, es, ts, ss), _), Source(Clean(n), Links(ls @ _*))) =>
         DiGraph(sgs, es ++ ls.map(Edge(_, n)), ts ++ ls.map(Topic(_)), ss) -> Some(n)
-      case ((DiGraph(sgs, es, ts, ss), _), Proc(Clean(n), Links(ls @ _*))) =>
+      case ((DiGraph(sgs, es, ts, ss), _), Processor(Clean(n), Links(ls @ _*))) =>
         DiGraph(sgs, es ++ ls.map(Edge(n, _)), ts, ss ++ ls.map(Store(_))) -> Some(n)
       case ((DiGraph(sgs, es, ts, ss), _), Sink(Clean(n), Links(l))) =>
         DiGraph(sgs, es :+ Edge(n, l), ts + Topic(l), ss) -> Some(n)
-      case ((DiGraph(Subgraph(id, la, sges) :: sgs, es, ts, ss), Some(n)), Arrow(Links(ls @ _*))) =>
+      case ((DiGraph(Subgraph(id, la, sges) :: sgs, es, ts, ss), Some(n)), RightArrow(Links(ls @ _*))) =>
         DiGraph(Subgraph(id, la, sges ++ ls.map(Edge(n, _))) :: sgs, es, ts, ss) -> Some(n)
       case (acc, _) =>
         acc
